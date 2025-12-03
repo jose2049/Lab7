cd70808a51f8946da12bfb709436ee990b490df0
Jose Alvarez <g00463862@atu.ie>
2025-12-03 11:04:53 +0000

    adding more comments to code + creating a new class called PreferredCustomerClass that inherits fields from customer and adds 2 new field loyalty points and discount

--- src/main/java/atu/ie/CustomerClass.java
+++ src/main/java/atu/ie/CustomerClass.java
@@ -1,29 +1,29 @@
 package atu.ie;
 
-import java.util.Objects;
-
-public class CustemerClass extends PersonClass {
+public class CustomerClass extends PersonClass {
     private int customerNumber;
     private boolean mailingList;
 
-    //Default Constructor
-    public CustemerClass() {
-        super();
-        this.customerNumber = 0;
-        this.mailingList = false;
-    }
-    //Constructor with initialized parameters
-    public CustemerClass(int customerNumber, boolean mailingList) {
-        this.customerNumber = customerNumber;
-        this.mailingList = mailingList;
-    }
-    //Calling the parent constructor from PersonClass
-    public CustemerClass(String name, String address, String phoneNumber, int customerNumber, boolean mailingList) {
+    //Parameterized constructor from PersonClass
+    public CustomerClass(String name, String address, String phoneNumber, int customerNumber, boolean mailingList) {
         super(name, address, phoneNumber);
         this.customerNumber = customerNumber;
         this.mailingList = mailingList;
     }
+    //Parameterized constructor for CustomerClass + calling the parent constructor using super
+    public CustomerClass(int customerNumber, boolean mailingList) {
+        super();
+        this.customerNumber = customerNumber;
+        this.mailingList = mailingList;
+    }
+    //No argument constructor
+    public CustomerClass() {
+        super();
+        this.customerNumber = 0;
+        this.mailingList = false;
+    }
 
+    //setting sets and getters
     public int getCustomerNumber() {
         return customerNumber;
     }
@@ -40,6 +40,7 @@ public class CustemerClass extends PersonClass {
         this.mailingList = mailingList;
     }
 
+    //Returning a readable representation
     @Override
     public String toString() {
         return "Customer{" +
--- src/main/java/atu/ie/CustomerTest.java
+++ src/main/java/atu/ie/CustomerTest.java
@@ -1,34 +1,33 @@
 package atu.ie;
-
-import java.util.Scanner;
+//I'm constructing Customer objects in 2 different ways
+//1 all values at once via the constructor
+//2 Step by step via setters
+//In both cases, the Person part is still initialised because of inheritance and super
 
 public class CustomerTest {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        //Creating a parameterized constructor
         PersonClass person = new PersonClass( "José", "Galway" ,"1234");
         System.out.println(person);
 
-        CustemerClass customer = new CustemerClass("José", "Galway" ,"1234",5678, true);
-        System.out.println(customer);
-
-
-        CustemerClass part1Customer = new CustemerClass(
-                "Alice Smith",
-                "1 High Street",
-                "0851234567",
-                1001,
+        CustomerClass part1Customer = new CustomerClass(
+                "José",
+                "Galway" ,"1234",
+                5678,
                 true
         );
 
-        CustemerClass part2Customer = new CustemerClass();
+        System.out.println(part1Customer);
+
+        //No-argument constructor. Using setters to assign values
+        CustomerClass part2Customer = new CustomerClass();
         part2Customer.setName("Bob Jones");
         part2Customer.setAddress("2 Low Street");
         part2Customer.setPhoneNumber("0867654321");
         part2Customer.setCustomerNumber(1002);
         part2Customer.setMailingList(false);
 
+        System.out.println(part2Customer);
     }
-
 }
